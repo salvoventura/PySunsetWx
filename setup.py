306707f761f6a0c5c67706fcd4065b4714c04364
salvoventura <venturasalvatore@yahoo.it>
2019-06-30 19:55:48 -0700

    - Fixed setup.py for submission to PyPi

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
                       # that you indicate whether you support Python 2, Python 3 or both.
                       'Programming Language :: Python :: 2.7',
                       'Programming Language :: Python :: 3.5',
-                      'Programming Language :: Python :: 3.6'
+                      'Programming Language :: Python :: 3.6',
                       'Programming Language :: Python :: 3.7'
                   ],
     keywords=['pysunsetwx', 'rest', 'api', 'python', 'wrapper', 'development', 'sunsetwx.com', 'photography'],
